ebb9785558175fc9aeeb9b01ed8d10cdd5d3eebb
Phil <philip-lisak@lambdastudents.com>
2020-10-02 20:07:22 -0700

    Still having issues

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 
 import App from './App';
 import { fetchShow as mockFetchShow } from './api/fetchShow';
 import { episodeFixture } from './components/Episodes.test';
+import userEvent from '@testing-library/user-event';
 
 jest.mock('./api/fetchShow')
 
+
 test('App renders without crashing', () => {
     render(<App />)
+})
+
+test('Displays dropdown list of options when Select Season is clicked', async () => {
+    mockFetchShow.mockResolvedValueOnce({ data: episodeFixture })
+
+    const { queryByTestId } = render(<App />);
+    const dropdown = queryByTestId('dropdown');
+
+    await waitFor(userEvent.click(dropdown))    
+    await waitFor(() => {
+        expect(dropdown.length).toBe(4);
+    })
 })
--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, getByText } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import Episodes from './Episodes';
 
